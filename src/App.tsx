87b1304cd59969d60d46083023dd6345db95d44a
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-08-27 10:57:24 -0300

    feat(class5-exercise1): add RemoveNames component for managing a list of names

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ButtonILke from "./Class3/Exercise2-I like/ButtonILike"
 import TrafficLight from "./Class4/TrafficLight"
 import StockAlert from "./Class4/StockAlert";
 import ListOfNames from "./Class5/ListOfNames";
+import RemoveNames from "./Class5/RemoveNames"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <TrafficLight/>
       <StockAlert/>
       <ListOfNames/>
+      <RemoveNames/>
       
     </div>
 
