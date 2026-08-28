9110be175ed2487f0f477f4b0ed9f987c434f21a
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-08-28 11:50:01 -0300

    feat(class10): implement Contador context and Count component for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import Count from "./Class3/Exercise1-Count/Count"
-import ButtonILke from "./Class3/Exercise2-I like/ButtonILike"
-import TrafficLight from "./Class4/TrafficLight"
-import StockAlert from "./Class4/StockAlert";
-import ListOfNames from "./Class5/ListOfNames";
-import RemoveNames from "./Class5/RemoveNames"
+
+import {ContadorProvider} from "./Class10/Context";
+import CountContext from "./Class10/CountContext"
 
 function App() {
 
   return (
     <div>
-      <Count />
-      <ButtonILke />
-      <TrafficLight/>
-      <StockAlert/>
-      <ListOfNames/>
-      <RemoveNames/>
       
+      <ContadorProvider>
+        <CountContext />
+      </ContadorProvider>
+
     </div>
 
   )
